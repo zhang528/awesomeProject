70df4547c1d1212bf0b676183553e295fc28dab8
zhangjunfeng.520 <zhangjunfeng.520@bytedance.com>
2022-12-28 20:06:41 +0800

    third commit

--- test3/main.go
+++ test3/main.go
@@ -72,3 +72,16 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	var next *ListNode
+	var cur = head
+	for cur != nil {
+		next = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
